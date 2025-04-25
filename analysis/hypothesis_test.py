87ab1c972905b01f727ec2e8b369e1db72d82a45
bbenduylu <beyza.benduylu@sabanciuniv.edu>
2025-04-25 16:48:49 +0300

    Update hypothesis_test.py

--- analysis/hypothesis_test.py
+++ analysis/hypothesis_test.py
@@ -11,3 +11,21 @@ def zamanduzelt(t):
 
 df["Kolay Süre"]= df["Kolay Ortalama Tamamlama Süresi (dakika)"].apply(zamanduzelt)
 df["Zor Süre"] = df["Zor Ortalama Tamamlama Süresi (dakika)"].apply(time_to_float)
+
+#hypothesis 1--- comp. of easy and hard rates
+easywrates= df["Kolay Kazanma Oranı (%)"]
+hardwrates=df["Zor Kazanma Oranı (%)"]
+
+#t-test
+t_num, p_value = ttest_ind(easywrates, hardwrates, equal_var=False)
+
+print("Hypothesis 1 - Easy vs Hard Win Rate Comparison")
+print("t statistic: " + str(round(t_num, 3)))
+print("p-value: " + str(round(p_value,5)))
+
+if p_value<0.05:
+  print("Conclusion: There is a statistically significant difference between the win rates (reject H0).")
+else:
+    print("Conclusion: No statistically significant difference found (fail to reject H0).")
+
+                         
